100be4a68a1df946761e3b902d4660c52254b0c8
Chihyun Song <ian0371@gmail.com>
2023-07-20 18:19:25 +0900

    Improve temporary log

--- consensus/istanbul/core/vrank.go
+++ consensus/istanbul/core/vrank.go
@@ -1,25 +1,22 @@
-// Modifications Copyright 2018 The klaytn Authors
-// Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// Copyright 2023 The klaytn Authors
+// This file is part of the klaytn library.
 //
-// The go-ethereum library is free software: you can redistribute it and/or modify
+// The klaytn library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The go-ethereum library is distributed in the hope that it will be useful,
+// The klaytn library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-//
-// This file is derived from quorum/consensus/istanbul/core/commit.go (2018/06/04).
-// Modified and improved for the klaytn development.
+// along with the klaytn library. If not, see <http://www.gnu.org/licenses/>.
 package core
 
 import (
+	"fmt"
 	"math/big"
 	"time"
 
@@ -90,7 +87,12 @@ func vrankAtPreprepare(view *istanbul.View) {
 		    LastCommitArrivalTimeMetrics.Update(lastCommit)
 	*/
 
-	logger.Info("VRank", "vrankCommitArrivalTimeMap", vrankCommitArrivalTimeMap)
+	log := "[ "
+	for k, v := range vrankCommitArrivalTimeMap {
+		log += fmt.Sprintf("%s:%s ", k.Hex(), v)
+	}
+	log += "]"
+	logger.Info("VRank", "vrankCommitArrivalTimeMap", log)
 	lateCommits := filterLateCommits(vrankCommitArrivalTimeMap)
 	if view.Round.Cmp(common.Big0) == 0 {
 		// TODO-VRANK: encode
